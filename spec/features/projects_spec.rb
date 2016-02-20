9e3fa852b98b9a89d2e905805794374dc6aa6ba1
Oleg Zaporozhchenko <olegz@jetruby.com>
2016-02-20 14:10:57 +0000

    Fixed test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Projects Page', type: :feature do
 
-
   describe 'Index Page' do
     let!(:project) { create :project }
 
     before { visit projects_path }
+
+    it 'should display project title' do
+      expect(page).to have_content project.title
+    end
 
     it 'should display project title' do
       expect(page).to have_content project.title
